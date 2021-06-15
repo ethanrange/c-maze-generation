4b36ea0b79f33c93dc003b28a93efcdc386d38e4
Ethan Range <65268454+ethanrange@users.noreply.github.com>
2021-06-15 02:08:34 +0100

    Implemented backtracking and major refactor
    (*) Implemented recursive backtracking maze generation algorithm
    (*) Split extension into a distinct files for separate algorithms
    (*) Updated Makefile to correctly compile

---
+++ extension/src/kruskals.h
@@ -0,0 +1,11 @@
+#include "constants.h"
+#include "raylib.h"
+#include "utils.h"
+
+#include <string.h>
+
+void init_kruskals(Tile tile[ROWS][COLS]);
+
+int step_kruskals(Tile tile[ROWS][COLS]);
+
+void run_kruskals(Tile tile[ROWS][COLS]);
